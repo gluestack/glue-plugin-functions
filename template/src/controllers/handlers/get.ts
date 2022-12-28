87ca594b0039f06f381c13abb0e25a7c91775751
Faiz A. Farooqui <faizz.af@gmail.com>
2022-12-28 13:19:56 +0530

    chore: made fixes in the server config, docker file & added readme file

--- template/src/controllers/handlers/get.ts
+++ template/src/controllers/handlers/get.ts
@@ -2,8 +2,6 @@ import Common from '../commons';
 
 class Get {
   public static async handle(req: any, res: any): Promise<void> {
-    const { } = req.body.input.input || req.body;
-
     try {
       return Common.Response(res, true, "success!", {});
     } catch (error) {
