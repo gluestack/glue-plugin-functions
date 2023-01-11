7f4f00eb696c21a70264a817f553bcdcfc31c65e
Faiz A. Farooqui <faizz.af@gmail.com>
2023-01-11 10:33:12 +0530

    chore(v0.0.25): updated template with latest changes

--- template/server.js
+++ template/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 9000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/health-check', (req, res) => {
   return res.status(200).json({status: true, message: 'Ok'});
 });
 
+app.post('/webhook', (req, res) => {
+  const body = req.body;
+
+  // do something with the body here
+
+  return res.status(200).json({status: true, message: 'done'});
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Function app listening on port ${port}`)
 });
