55fd69b9bd7c34e6d23513b0b641fb3ea83f4832
Faiz A. Farooqui <faizz.af@gmail.com>
2023-01-16 14:45:45 +0530

    chore(v0.0.29): version with new changes

--- template/server.js
+++ template/server.js
@@ -11,18 +11,48 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/health-check', (req, res) => {
-  return res.status(200).json({status: true, message: 'Ok'});
+  return res.status(200).json({status: true, message: 'Health check performed successfully'});
 });
 
-app.post('/webhook', (req, res) => {
-  const body = req.body;
+const middleware = (req, res, next) => {
+  const { headers } = req;
 
-  // do something with the body here
-  console.log({ body });
+  if (! headers.hasOwnProperty('x-glue-invoke')) {
+    return next();
+  }
 
-  return res.status(200).json({status: true, message: 'done'});
+  const { 'x-glue-invoke': invoke } = headers;
+  const isPublic = process.env.GLUE_PUBLIC === 'false' ? false : true;
+  if (invoke !== 'client' || isPublic === true) {
+    return next();
+  }
+
+  //
+  // If GLUE_PUBLIC is set specifically to "false". This means that the function
+  // is not publically exposed to client invoke calls.
+  //
+  // If it is set "true" or the env var does not exist, then it is assumed that
+  // the function is available for client invoke calls.
+  return res.status(401).json({
+    status: false,
+    message: 'Access denied'
+  });
+};
+
+app.post('/functions', middleware, (req, res) => {
+  const { headers, body }  = req;
+
+  // do something with the headers and body
+  // perform your custom business logic
+
+  console.log({ headers, body });
+
+  return res.status(200).json({
+    status: true,
+    message: 'Ok'
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Function app listening on port ${port}`)
+  console.log(`Action listening on port ${port}`)
 });
